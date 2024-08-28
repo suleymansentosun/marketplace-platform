cdb76faadb8727f3af5029267a7d2f53863ae1a6
Ali Turkes <aliturkes57@gmail.com>
2024-08-28 15:58:14 +0200

    Review functionalities

--- db/database.py
+++ db/database.py
@@ -1,12 +1,15 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
  
 SQLALCHEMY_DATABASE_URL = "sqlite:///./marketplace-platform.db"
  
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
 )
+engine.connect().execute(text("PRAGMA foreign_keys=on"))
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
  
 Base = declarative_base()
